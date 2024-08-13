dbfdc43517147ea2eceb1ad95a28fdece57e1d9d
chaolen <siaotongcharlyn@gmail.com>
2024-08-13 14:46:30 +0800

    feat: add constant data

---
+++ src/constants/guests.js
@@ -0,0 +1,32 @@
+export const guestList = {
+    bridesParents: [
+        "Mr. Claudio L. Aparecio Jr.",
+        "Mrs. Juliet B. Aparecio"
+    ],
+    groomsParents: [
+        "Mr. Michael Q. Jorolan",
+        "Mrs. Lorena E. Jorolan"
+    ],
+    ninongs: [
+        "Mr.Joel M.Baldonado",
+        "Mr.Felipe J.Nazaire",
+        "Dr.Jose M.Baldonado Jr.",
+        "Mr.Rey B.Cordero",
+        "Mr.Norberto L.Aparicio",
+        "Engr.Aries D.Madelo",
+        "Mr.Feloteo B.Rimando",
+        "Mr.Ronaldo E.Narra"
+    ],
+    ninangs: [
+        "Mrs. Socorro B. Baldonado",
+        "Mrs. Nenita A. Nazaire",
+        "Mrs. Luz A. Donaire",
+        "Mrs. Helen P. Cordero",
+        "Mrs. Milagros D. Aparicio",
+        "Mrs. Ellaine Mary B. Madelo",
+        "Mrs. Merilyn E. Marfilo",
+        "Mrs. Marissa E. Villaflor",
+        "Mrs. Evangeline B. Narra",
+        "Mrs. Helen Maquiling"
+    ]
+}
