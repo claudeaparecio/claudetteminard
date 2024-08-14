c75d0f9bb407d3f4899a33b14aa2161bda7d0499
chaolen <siaotongcharlyn@gmail.com>
2024-08-15 05:05:13 +0800

    add constant data

---
+++ src/constants/faqs.js
@@ -0,0 +1,34 @@
+export const faqs = [
+    {
+        title: "Unsay oras ang kasal?",
+        description: "Siguraduha nasa simbahan na 1:45PM. Magsugod ang pag martsa sa entourage saktong 2:00PM. WALAY FILIPINO TIME!"
+    },
+    {
+        title: "Pwede ba sa reception nalang muadto?",
+        description: "Kung kaon ray apas pwede sad. Chariz lang! We encourage you to witness as we exchange our vows, but if your religion or schedule requires, feel free to go directly to the reception."
+    },
+    {
+        title: "Unsay ganap after ceremony?",
+        description: "Syempre ang imong gipaabot na kaon. CHAR! Pagkahuman sa seremonya, couple & entourage will be taking pictures for around an hour. The guests can transfer to the reception area and enjoy some snacks and drinks."
+    },
+    {
+        title: "Can I bring my kids or plus with me?",
+        description: "Pasensya jud kayo pero this is an INVITE-ONLY / ADULT ONLY event. Gustuhon man namo makauban tanang tawo samong kinabuhi anang adlawa apan we only have limited budget & resources. Guests not on the guest list will not be accommodated at the reception. THANK YOU FOR UNDERSTANDING!"
+    },
+    {
+        title: "Bisag asa pwede mulingkod sa reception?",
+        description: "Bisan pag sa salog. Jokejoke! Pero kung gusto ninyo sa tama na lingkuranan, aduna mi’y giandam na seat plan kung asa kuyog mo sa imong mga friends ug higala para dili OP ang beshy ko!"
+    },
+    {
+        title: "Unsay oras mahuman?",
+        description: `It took us months to save and plan for our lovely wedding that everyone would hopefully enjoy. That program is estimated to last until 8PM. PLEASE DON’T EAT AND RUN. (capslock na po) Stay with us until the end of the program!`
+    },
+    {
+        title: "Unsay dapat suoton?",
+        description: `Ang theme is ELEGANT RUSTIC GREENERY para nature lover. FORMAL OR SEMI-FORMAL. PAKISUNOD PO ANG DRESS CODE AT COLOR.`
+    },
+    {
+        title: "Unsay pwedeng iregalo?",
+        description: `Your presence at our wedding is gift enough. Pero kung mamugos jud ka. CASH. OPO. KWARTAHON NALANG NATO HAHAHA. Di mi magpa atik atik, mudawat rami’g box ug ang sulod kay Videocard! JOKE ONLY PO. As we begin our journey as husband and wife, dakong butang na para namo ang bisan unsa nga inyong maisip nga makatabang sa among pagsugod.`
+    }
+]
--- src/constants/guests.js
+++ src/constants/guests.js
@@ -28,5 +28,61 @@ export const guestList = {
         "Mrs. Marissa E. Villaflor",
         "Mrs. Evangeline B. Narra",
         "Mrs. Helen Maquiling"
+    ],
+    bestMan: [
+        "Reynaldo P. Gayonilo Jr."
+    ],
+    maidOfHonor: [
+        "Jeah Marie E. Jorolan",
+        "Elloisa Mary A. Bautista",
+        "Jeza Myrrh D. Mangaoang",
+    ],
+    matronOfHonor: [
+        "Jebecca A. Buenavista"
+    ],
+    groomsMen: [
+        "Claude Jacob B.Aparecio",
+        "Jenny D. Biñas",
+        "Thom Alfred R. Gerance",
+        "Dexter T. Lampon",
+        "John Arthem E. Dionela",
+        "Christian Gene G. Dequitos",
+    ],
+    bridesMaid: [
+        "Mia Joy Aiana J. Manulat",
+        "Chrisna Abbygail T. Decena",
+        "Princess Art A. Estrada",
+        "Aleca Aye O. Torreon",
+        "Rogelin L. Canda",
+        "Jenny B. Escalante",
+        "Phoebe Grace J. Bantayan",
+        "Chrisna Abbygail T. Decena",
+    ],
+    candle: [
+        "Michael P. Asidor",
+        "Vaniza A. Consuelo"
+    ],
+    cord: [
+        "Ronald B. Narra",
+        "Mary Jean S. Narra"
+    ],
+    veil: [
+        "Carlo C. Carbonilla",
+        "Norie Mae T. Pulvera"
+    ],
+    ringBearer: [
+        "Jeb Antonette A. Morator"
+    ],
+    coinBearer: [
+        "Louigie Miguel Telen"
+    ],
+    bibleBearer: [
+        "Niko Angelo R. Perez"
+    ],
+    flowerGirls: [
+        "Jaxine Mavie E. Jorolan",
+        "Larah Elizia A. Buenavista",
+        "Meccaila A. Buenavista",
     ]
+
 }
